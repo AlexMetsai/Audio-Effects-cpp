f79ab1abd6ab9e7828604fbf7977d89474cb2600
Alexandros Metsai <alexmetsai@gmail.com>
2020-09-08 19:05:20 +0300

    Update reverb.cpp

--- reverb.cpp
+++ reverb.cpp
@@ -1,1 +1,11 @@
+/* Alexandros I. Metsai
+ * alexmetsai@gmail.com
+ *
+ * License will probably be GPLv3.
+ *
+ * Librady for reading/writing wav files: adamstark/AudioFile
+ */
 
+int main(){
+    // TODO
+}
