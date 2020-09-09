c34fb69f6a996314c7d0b354f5b66100062f4881
Alexandros Metsai <alexmetsai@gmail.com>
2020-09-09 10:53:41 +0300

    Update reverb.cpp

--- reverb.cpp
+++ reverb.cpp
@@ -15,16 +15,24 @@
 AudioFile<double> delay_reverb(AudioFile<double> audioFile);
 
 int main(){
-    //TODO
+    // Load wav file
+    AudioFile<double> audioFile;
+    audioFile.load("input.wav");
+    
+    // Apply Reverb effect.
+    audioFile = delay_reverb(audioFile);
+    audioFile.save("output.wav");
+    
+    return 0;
 }
 
-// Algorithmic delay/reverb.
+// Algorithmic Delay/Reverb.
 AudioFile<double> delay_reverb(AudioFile<double> audioFile){
     // TODO: add decay and delay duration as parameters?
     float delayMilliseconds = 0.3;
     double decay = 0.5f;
     int delaySamples = int(delayMilliseconds * audioFile.getSampleRate());
-    std::cout << "Delay/Reverb effect using" << "delaySamples: " << delaySamples << " " << audioFile.getSampleRate() << " " << audioFile.getNumSamplesPerChannel();
+    std::cout << "Delay/Reverb effect using" << "delaySamples: " << delaySamples << " " << audioFile.getSampleRate() << " " << audioFile.getNumSamplesPerChannel() << std::endl;
     for (int i = 0; i < audioFile.getNumSamplesPerChannel() - delaySamples; i++){
         audioFile.samples[0][i+delaySamples] += (double)((double)audioFile.samples[0][i] * decay);
     }
