163be061742876e35374ce6a956874fbe4166218
Alexandros Metsai <alexmetsai@gmail.com>
2020-09-09 10:50:19 +0300

    Delay reverb implementation

--- reverb.cpp
+++ reverb.cpp
@@ -17,3 +17,16 @@ AudioFile<double> delay_reverb(AudioFile<double> audioFile);
 int main(){
     //TODO
 }
+
+// Algorithmic delay/reverb.
+AudioFile<double> delay_reverb(AudioFile<double> audioFile){
+    // TODO: add decay and delay duration as parameters?
+    float delayMilliseconds = 0.3;
+    double decay = 0.5f;
+    int delaySamples = int(delayMilliseconds * audioFile.getSampleRate());
+    std::cout << "Delay/Reverb effect using" << "delaySamples: " << delaySamples << " " << audioFile.getSampleRate() << " " << audioFile.getNumSamplesPerChannel();
+    for (int i = 0; i < audioFile.getNumSamplesPerChannel() - delaySamples; i++){
+        audioFile.samples[0][i+delaySamples] += (double)((double)audioFile.samples[0][i] * decay);
+    }
+    return audioFile;
+}
