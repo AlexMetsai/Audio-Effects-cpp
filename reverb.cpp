4b9e7ed4655e12bad15e4e0a5961a12c04a83253
Alexandros Metsai <alexmetsai@gmail.com>
2020-09-09 10:44:41 +0300

    function def and includes

--- reverb.cpp
+++ reverb.cpp
@@ -6,6 +6,14 @@
  * Librady for reading/writing wav files: adamstark/AudioFile
  */
 
+#include <vector>
+#include <iostream>
+#include <string>
+#include "AudioFile.h"
+#include <cstring>
+
+AudioFile<double> delay_reverb(AudioFile<double> audioFile);
+
 int main(){
-    // TODO
+    //TODO
 }
