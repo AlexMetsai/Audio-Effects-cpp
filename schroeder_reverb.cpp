25948ee28c97eff483870465d82c7757f789b7b1
Alexandros Metsai <alexmetsai@gmail.com>
2020-09-11 14:52:48 +0300

    function placeholders

--- schroeder_reverb.cpp
+++ schroeder_reverb.cpp
@@ -1,2 +1,12 @@
 // Implemented the more sophisticated reverb algorithm proposed by Manfred Schroeder.
 // This approach makes used of Comb and All-Pass filters.
+
+void combFilter();
+
+void allPassFilter();
+
+AudioFile<double> schroeder_reverb(AudioFile<double> audioFile);
+
+int main(){
+    return 0;
+}
