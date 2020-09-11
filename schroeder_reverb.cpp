440a9e44666109dc411b5a1a99992d7468ba7dbb
Alexandros Metsai <alexmetsai@gmail.com>
2020-09-11 15:23:15 +0300

    Comb Filter

--- schroeder_reverb.cpp
+++ schroeder_reverb.cpp
@@ -1,7 +1,7 @@
 // Implemented the more sophisticated reverb algorithm proposed by Manfred Schroeder.
 // This approach makes used of Comb and All-Pass filters.
 
-void combFilter();
+AudioFile<double> combFilter(AudioFile<double> audioFile, double delayMilliseconds, double decay);
 
 void allPassFilter();
 
@@ -9,4 +9,19 @@ AudioFile<double> schroeder_reverb(AudioFile<double> audioFile);
 
 int main(){
     return 0;
+}
+
+AudioFile<double> combFilter(AudioFile<double> audioFile, double delayMilliseconds, double decay){
+
+    // Calculate the number of delay samples.
+    int delaySamples = int(delayMilliseconds * (audioFile.getSampleRate()/1000));
+
+    // See if you need a separate copy of 'audioFile'.
+    // temp = pseudocopy audioFile;
+
+    for (int i = 0; i < audioFile.getNumSamplesPerChannel() - delaySamples; i++{
+        audioFile.samples[0][i+delaySamples] += (double)((double)audioFile.samples[0][i] * decay);
+        // DO THIS FOR ALL CHANNELS THAT THE WAV FILE PROVIDES.
+    }
+    return audioFile;
 }
